e343e796bf39f7710b58cae510146dacd1f0de36
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-09-20 14:59:05 +0300

    Create __init__.py

---
+++ src/minesweeper/__init__.py
@@ -0,0 +1,11 @@
+"""
+Quantum Minesweeper Module.
+Exposes the QuantumMinesweeperField for managing probabilistic mine layouts,
+superposition initialization, and spatial wave function collapse mechanics.
+"""
+
+from src.minesweeper.field import QuantumMinesweeperField
+
+__all__ = [
+    "QuantumMinesweeperField",
+]
